89b7fbfc48bf8fdfb0adc334199473c098b8e971
Felix Penrose <felix.p@oliverstravels.com>
2023-03-10 12:13:30 +0000

    feat(laravel-upgrade): initialising shared repo for php styles
    OT-33

---
+++ src/php-cs-fixer/rules.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    '@PSR12' => true,
+    'concat_space' => [
+        'spacing' => 'one',
+    ],
+    'class_attributes_separation' => [
+        'elements' => [
+            'const' => 'none',
+            'method' => 'one',
+            'property' => 'one',
+            'trait_import' => 'none',
+        ],
+    ],
+];
